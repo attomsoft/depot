ba94b1e005df5231513a4b2a976b513558ef9080
attomsoft <noabuse@attomsoft.com>
2014-04-02 02:57:11 +0800

    cart works
    maybe buggy, but works for now. Using application level cart object is
    not good choice.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :get_cart
+  private
+  def current_cart
+  	Cart.find(session[:cart_id])
+  rescue	ActiveRecord::RecordNotFound
+  	cart = Cart.create
+  	session[:cart_id] = cart.id
+  	cart
+  end
+
+  public
+  def get_cart
+  	@my_cart = current_cart
+  end
 end

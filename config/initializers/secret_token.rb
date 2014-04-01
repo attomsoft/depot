486c2b497b8b9f8545c8a7c4235010fb6acd8383
Attomsoft <noabuse@attomsoft.com>
2014-04-01 17:13:59 +0800

    Init adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '98fe82744c442fe8f0edf074cbb908cc7bda1dad367847bd2df34a1450c4cd1a06df5d8fb4e3d607ceb1c1d3b05a3a3210675a027b676bb77c77d047d0418be0'

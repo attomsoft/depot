fe9c4347fa9fc0815e1ff6c65a9b7fb3961c34df
Attomsoft <noabuse@attomsoft.com>
2014-04-02 16:08:00 +0800

    smarter, securer cart.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :get_cart
+  before_filter :current_cart
   private
   def current_cart
   	Cart.find(session[:cart_id])
@@ -13,7 +13,4 @@ class ApplicationController < ActionController::Base
   end
 
   public
-  def get_cart
-  	@my_cart = current_cart
-  end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,7 +10,7 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
-    @cart = current_cart
+
   end
 
   # GET /carts/new
@@ -55,9 +55,14 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
+
+    # destroy only current seesion cart
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
+
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
@@ -65,7 +70,12 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.find(params[:id])
+      begin
+        @cart = Cart.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        logger.error "Attempt to access invalid cart #{params[:id]}"
+        redirect_to store_url, notice: "Invalid cart"
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
